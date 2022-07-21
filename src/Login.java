63dca5affe1340eba2f7f4bcbc9518449d9edc81
Hackim2 <hakim2001@hotmail.de>
2022-07-21 20:03:32 +0200

    second

--- src/Login.java
+++ src/Login.java
@@ -4,45 +4,54 @@ import java.awt.event.*;
 
 public class Login implements ActionListener {
     JFrame frame = new JFrame();
-    JButton mbutton = new JButton("Mitarbeiter");
-    JButton abutton = new JButton("Aufträge");
+    JButton mitarbeiter_button = new JButton("Mitarbeiter");
+    JButton aufträge_button = new JButton("Aufträge");
 
 
-    Login(){
-        //MitarbeiterButton
-        mbutton.setBounds(50, 125, 100, 50);
-        mbutton.setFocusable(false);
-        mbutton.addActionListener(this);
-        //AufträgeButton
-        abutton.setBounds(250, 125, 100, 50);
-        abutton.setFocusable(false);
-        abutton.addActionListener(this);
-
+    Login() {
+        createMitarbeiterButton();
+        createAuftragButton();
+        createFrame();
 
 
         //Frame
+
+
+    }
+
+    public void createFrame() {
         frame.setTitle("Menü");
-        frame.add(mbutton);
-        frame.add(abutton);
+        frame.add(mitarbeiter_button);
+        frame.add(aufträge_button);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 300);
         frame.setLayout(null);
         frame.setVisible(true);
+    }
 
+    public void createMitarbeiterButton() {
+        mitarbeiter_button.setBounds(50, 125, 100, 50);
+        mitarbeiter_button.setFocusable(false);
+        mitarbeiter_button.addActionListener(this);
+    }
+
+    public void createAuftragButton() {
+        aufträge_button.setBounds(250, 125, 100, 50);
+        aufträge_button.setFocusable(false);
+        aufträge_button.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == mbutton) {
+        if (e.getSource() == mitarbeiter_button) {
             frame.dispose();
-            Mitarbeiter Mitarbeiter = new Mitarbeiter();
-        }
-        else if(e.getSource()==abutton){
+        } else if (e.getSource() == aufträge_button) {
             frame.dispose();
             Aufträge Aufträge = new Aufträge();
 
             // TODO : open Aufträge
-            }
+        }
 
     }
 }
+
